6f17769e886a307e10cfa62603271c60ca30850b
Spencer Veatch <scveatch@willamette.edu>
2026-05-09 21:46:13 -0700

    Solve 5

---
+++ problems/0005-smallest-multiple/0005_solution.py
@@ -0,0 +1,39 @@
+"""
+File: 0005_solution.py
+
+Description: Project Euler Problem 5: Smallest Multiple
+
+Author: Spencer Veatch (sveatch@willamette.edu)
+
+Last Modified: 05/09/2026
+"""
+import math
+import functools
+
+def least_common_mult(first: int, second: int) -> int: 
+    """
+    Constructs the least common multiple of two values `first` and `second`.
+
+    Args:
+        first (int): The first integer to be evaluated.
+        second (int): The second integer to be evaluated.
+
+    Returns:
+        (int): The least common multiple of the two provided values.
+    """
+    return abs(first * second) // math.gcd(first, second)
+
+def smallest_multiple(limit: int) -> int: 
+    """
+    Gets the smallest multiple up to the provided `limit` (inclusive).
+
+    Args:
+        limit (int): The value at which to stop evaluation.
+
+    Returns:
+        (int): The smallest multiple of all values up to and including `limit`.
+    """
+    return functools.reduce(least_common_mult, range(1, limit + 1))
+
+if __name__ == "__main__":
+    print(smallest_multiple(20))
