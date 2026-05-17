1f2371f88048407443c2289e400cdff278c59f6e
Spencer Veatch <scveatch@willamette.edu>
2026-05-16 21:28:07 -0700

    Start work / notes

---
+++ problems/0989-fibonacci-sum/0989_solution.py
@@ -0,0 +1,17 @@
+"""
+File: 0989_solution.py
+
+Description: Project Euler Problem 989: Fibonacci Sum
+
+Author: Spencer Veatch (sveatch@willamette.edu)
+
+Last Modified: 05/09/2026
+"""
+
+
+def main() -> None:
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
